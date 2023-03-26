db9ec1827c506f53ea42c490fcfabd0171627c84
Пустовий Борис <107931829+BorisPustovyi@users.noreply.github.com>
2023-03-26 13:42:32 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,7 @@
 const itemRef = document.querySelectorAll(".item");
-console.log(`Number of categories:`, itemRef.length)
-itemRef.forEach(el => )
+console.log(`Number of categories:`, itemRef.length);
+itemRef.forEach(el =>
+{
+    console.log(el.firstElementChild.textContent);
+        console.log(`Elements:`, el.lastElementChild.children.length)
+})
