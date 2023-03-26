a556a8157b773e1187952540cae03acd61949c0a
Пустовий Борис <107931829+BorisPustovyi@users.noreply.github.com>
2023-03-26 14:11:58 +0300

    2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ulEl = document.querySelector("#ingredients");
+for (const ingredient of ingredients) { 
+  const liEl = document.createElement("li");
+  liEl.textContent = ingredient;
+  ulEl.appendChild(liEl); 
+}
+
