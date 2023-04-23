0fa81ebed645cd8354d92580c580ce540f9677f4
liarSaderik <107931829+BorisPustovyi@users.noreply.github.com>
2023-04-23 19:47:43 +0300

    8-9-10

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", onSumbit);
+function onSumbit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill the all fields");
+  }
+  const userValues = {
+    email: email.value,
+    password: password.value,
+  };
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+button.addEventListener("click", changeColor);
+
+function changeColor(event) {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  span.innerText = color;
+  console.log(color);
 }
