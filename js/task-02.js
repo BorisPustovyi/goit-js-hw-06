4f70b8c4a8325df91aac4781d28024221329f5dd
liarSaderik <107931829+BorisPustovyi@users.noreply.github.com>
2023-04-30 00:12:37 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,15 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
 const ulEl = document.querySelector("#ingredients");
-for (const ingredient of ingredients) { 
+
+for (const ingredient of ingredients) {
   const liEl = document.createElement("li");
   liEl.textContent = ingredient;
-  ulEl.appendChild(liEl); 
+  ulEl.appendChild(liEl);
 }
-
+document.body.appendChild(ulEl);
